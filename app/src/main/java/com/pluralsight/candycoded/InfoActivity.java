986c523cc56e2a9361dec04e62956dda34746ff9
Phancie <INCORRECT97>
2019-10-25 08:19:26 +0000

    added phone intent

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -41,4 +41,10 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:0246748016"));
+        startActivity(intent);
+
+    }
 }
